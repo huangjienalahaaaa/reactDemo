6c65928c310b259a4bc0e89a6741f6a726e1cdee
hj <137612270@qq.com>
2020-04-23 09:05:52 +0800

    1.17

--- src/XiaojiejieItem.js
+++ src/XiaojiejieItem.js
@@ -6,6 +6,23 @@ class XiaojiejieItem extends Component {
     super(props);
     this.handleClick = this.handleClick.bind(this);
   }
+  /*
+      ２．使用shouldComponentUpdate来解决频繁渲染问题
+    */
+  // shouldComponentUpdate() {
+  //   //return false=>这个时候在input框中写入东西并且按按钮可以新增，也可以阻止频繁渲染问题．但是如果是修改里面的数据的话，就会报错，所以不能使用这个方法．所以得用下面shouldComponentUpdate方法（带有nextProps,nextState参数）
+  //   return false;
+  // }
+  shouldComponentUpdate(nextProps, nextState) {
+    // nextProps，nextState->表示将要变化的属性，状态
+    if (nextProps.content !== this.props.content) {
+      //如果传递过来的content有改变，就发生渲染
+      return true;
+    } else {
+      //如果传递过来的content没有发生变化，就不渲染
+      return false;
+    }
+  }
   render() {
     return <li onClick={this.handleClick}>{this.props.content}</li>;
   }
--- src/xiaojiejie.js
+++ src/xiaojiejie.js
@@ -1,3 +1,9 @@
+/*
+ 1. 小姐姐组件存在性能问题：，那就是子组件XiaojiejieItem频繁XiaojiejieItem组件(无用渲染render)
+ 　＊　那么如何去解决呢？->在XiaojiejieItem.js中使用shouldComponentUpdate就能解决
+
+ */
+
 import React, { Component, Fragment } from "react";
 import XiaojiejieItem from "./XiaojiejieItem";
 class Xiaojiejie extends Component {
@@ -17,17 +23,12 @@ class Xiaojiejie extends Component {
             id="jspang"
             value={this.state.inputValue}
             onChange={this.inputChange.bind(this)}
-            // 1.定义ref:如果要使用ref来进行，需要现在JSX中进行绑定， 绑定时最好使用ES6语法中的箭头函数，这样可以简洁明了的绑定DOM元素。
             ref={(input) => {
               this.input = input;
             }}
           />
           <button onClick={this.addList.bind(this)}> 增加服务 </button>
         </div>
-        {/* ３．使用中的坑：
-         * 比如现在我们要用ref绑定取得要服务的数量，可以先用ref进行绑定，入下代码
-         * 绑定后可以在addList()方法中，获取当前<div>的值.
-         */}
         <ul
           ref={(ul) => {
             this.ul = ul;
@@ -48,49 +49,21 @@ class Xiaojiejie extends Component {
       </Fragment>
     );
   }
-  /*
-    2.因为上面定义了ref,所以下面的函数，不需要参数e,所以里面的e.target.value可以直接用this.input.value代替．（以前的案例中，我们写了下面的代码，使用了e.target，这并不直观，也不好看。这种情况我们可以使用ref来进行解决）
-  */
-  // inputChange(e) {
-  //   this.setState({
-  //     inputValue: e.target.value,
-  //   });
-  // }
   inputChange() {
     this.setState({
       inputValue: this.input.value,
     });
   }
-  //   addList() {
-  //     this.setState({
-  //       list: [...this.state.list, this.state.inputValue],
-  //       inputValue: "",
-  //     });
-
-  //     /*
-  //  4.绑定后可以在addList()方法中，获取当前<div>的值，如下代码．
-  //   console.log(this.ul.querySelectorAll("div").length);
-  //     * 问题：
-  //     这时候你打开控制台，点击添加服务按钮，你会返现数量怎么少一个？（就是这个坑），其实这个坑是因为React中更多setState是一个异步函数所造成的。也就是这个setState，代码执行是有一个时间的，如果你真的想了解清楚，你需要对什么是虚拟DOM有一个了解。简单的说，就是因为是异步，还没等虚拟Dom渲染，我们的console.log就已经执行了。
-
-  //     * 解决：
-  //     那这个代码怎么编写才会完全正常那，其实setState方法提供了一个回调函数，也就是它的第二个函数。下面这样写就可以实现我们想要的方法了。如下面的addList()函数
-  // */
-  //     console.log(this.ul.querySelectorAll("div").length);
-  //   }
-
   addList() {
     this.setState(
       {
         list: [...this.state.list, this.state.inputValue],
         inputValue: "",
-        //关键代码--------------start
       },
       () => {
         console.log(this.ul.querySelectorAll("div").length);
       }
     );
-    //关键代码--------------end
   }
   deleteItem(index) {
     let list = this.state.list;
